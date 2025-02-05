a366a3c2374332d06574ab8f382377041ba20a26
Migel <vtufa@yandex.ru>
2025-02-05 22:33:28 +0500

    bot code added2

---
+++ echo_bot.py
@@ -0,0 +1,31 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.types import (KeyboardButton, ReplyKeyboardRemove, ReplyKeyboardMarkup)
+BOT_TOKEN = '6844274696:AAHl9QFBXAQgjD8GhwY77Okhz-LQLxzzpQg'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer(
+        'Hi! My hame is Echo-bot!'
+    )
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'White me something and i will send the reply'
+    )
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text='bugaga')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
